ddb7b82b6da19fe7fd031fb86b33b2cb3f38050c
Prerit Mathur <mathur.prerit@gmail.com>
2020-06-22 01:34:45 +0530

    setup redux

---
+++ src/reducer/reducer.js
@@ -0,0 +1,12 @@
+const initalState = {
+    item:[]
+};
+
+export const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case "1":
+      return state;
+    default:
+      return state;
+  }
+};
